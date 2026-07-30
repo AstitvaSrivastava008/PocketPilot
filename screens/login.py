37423ef9156d3f7e65ae6e75e33c3972fc53fe66
AstitvaSrivastava008 <148468534+AstitvaSrivastava008@users.noreply.github.com>
2026-07-30 09:23:58 +0530

    Add advanced login validation

--- screens/login.py
+++ screens/login.py
@@ -6,8 +6,25 @@ class LoginScreen(Screen):
     def login(self):
         print("Login button clicked!")
 
-        email = self.ids.email_input.text
-        password = self.ids.password_input.text
+        email = self.ids.email_input.text.strip()
+        password = self.ids.password_input.text.strip()
 
-        print("Email:", email)
-        print("Password:", password)
+        if not email:
+            print("Please enter your email")
+
+        elif not password:
+            print("Please enter your password")
+
+        elif "@" not in email:
+            print("Please enter a valid email")
+
+        elif "." not in email:
+            print("Please enter a valid email")
+
+        elif len(password) < 8:
+            print("Password must be at least 8 characters long")
+
+        else:
+            print("Login Successful!")
+            print("Email:", email)
+            print("Password:", password)
